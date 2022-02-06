37cf45fd4b9e46afe2f8efea403328071a17ab66
Philipp Friedrich <freezer05@googlemail.com>
2022-02-06 14:53:10 +0100

    funktionierendes rechtes Paddel

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let linkesPaddel = document.getElementById("links");
 let rechtesPaddel = document.getElementById("rechts");
 let pressedOben = false;
 let pressedUnten = false;
+const paddelSpeed = 8;
 
 document.addEventListener("keydown", untendruecker);
 document.addEventListener("keyup", obenlasser);
@@ -27,16 +28,32 @@ function obenlasser(e) {
 setInterval(update, 16.66666666);
 
 function update() {
-  const style = getComputedStyle(rechtesPaddel);
-  console.log(pressedOben, pressedUnten);
-
-  if (pressedUnten) {
-    const alteHoehe = parseFloat(style.top.substring(0, style.top.length - 2));
-    const neueHoehe = alteHoehe + 10 + "px";
-    rechtesPaddel.style.top = neueHoehe;
-  } else if (pressedOben) {
-    const alteHoehe = parseFloat(style.top.substring(0, style.top.length - 2));
-    const neueHoehe = alteHoehe - 10 + "px";
-    rechtesPaddel.style.top = neueHoehe;
+  if (pressedUnten && !pressedOben) {
+    const style = getComputedStyle(rechtesPaddel);
+    const alteHoehe = getHoeheOhnePx(style.top);
+    const neueHoehe = alteHoehe + paddelSpeed;
+    const vh = window.innerHeight;
+    const paddelHoehe = getHoeheOhnePx(style.height);
+    if (paddelHoehe + alteHoehe < vh) {
+      const neueHoeheMitPixel = neueHoehe + "px";
+      rechtesPaddel.style.top = neueHoeheMitPixel;
+    } else {
+      rechtesPaddel.style.top = vh - paddelHoehe + "px";
+    }
   }
+  if (pressedOben && !pressedUnten) {
+    const style = getComputedStyle(rechtesPaddel);
+    const alteHoehe = getHoeheOhnePx(style.top);
+    const neueHoehe = alteHoehe - paddelSpeed;
+    if (neueHoehe >= 0) {
+      const neueHoeheMitPixel = neueHoehe + "px";
+      rechtesPaddel.style.top = neueHoeheMitPixel;
+    } else {
+      rechtesPaddel.style.top = "0px";
+    }
+  }
+}
+
+function getHoeheOhnePx(hoehe) {
+  return parseFloat(hoehe.substring(0, hoehe.length - 2));
 }
