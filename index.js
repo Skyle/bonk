0ed5278700837e240652e0f6b1735623d583efea
Philipp Friedrich <freezer05@googlemail.com>
2022-02-08 21:18:26 +0100

    spielbar

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let linkesPaddel = {
   element: document.getElementById("links"),
-  pos: { x: 0, y: 0 },
-  vec: { x: 0, y: 0 },
+  pos: { y: 0 },
+  vec: { y: 0 },
 };
 let rechtesPaddel = {
   element: document.getElementById("rechts"),
-  pos: { x: 0, y: 0 },
-  vec: { x: 0, y: 0 },
+  pos: { y: 0 },
+  vec: { y: 0 },
 };
 let ball = {
   element: document.getElementById("ball"),
@@ -18,25 +18,34 @@ let runningCheckbox = document.getElementById("running");
 let resetButton = document.getElementById("reset");
 resetButton.onclick = () => {
   resetAll();
+  stop();
+  start();
 };
 
 let framesInterval;
 runningCheckbox.onclick = (e) => {
   if (e.target.checked) {
-    start();
-    framesInterval = setInterval(update, 16.66666666);
+    resume();
   } else {
-    clearInterval(framesInterval);
-    console.log("aus");
+    pause();
   }
 };
 
-window.onload = () => {
-  init();
-};
 let pressedOben = false;
 let pressedUnten = false;
-const paddelSpeed = 8;
+
+// Globale Variablen
+const paddelSpeed = 12;
+const paddelSize = 128;
+const padding = 8;
+const ballSize = 24;
+const xSpeedUp = 1.05;
+const fpsFaktor = 16.666666666666668;
+
+window.onload = () => {
+  init();
+  start();
+};
 
 document.addEventListener("keydown", untendruecker);
 document.addEventListener("keyup", obenlasser);
@@ -72,28 +81,18 @@ function update() {
 
 function moveRechtesPaddel() {
   if (pressedUnten && !pressedOben) {
-    const style = getComputedStyle(rechtesPaddel);
-    const alteHoehe = removePixel(style.top);
-    const neueHoehe = alteHoehe + paddelSpeed;
-    const vh = window.innerHeight;
-    const paddelHoehe = removePixel(style.height);
-    if (paddelHoehe + alteHoehe < vh) {
-      const neueHoeheMitPixel = neueHoehe + "px";
-      rechtesPaddel.style.top = neueHoeheMitPixel;
-    } else {
-      rechtesPaddel.style.top = vh - paddelHoehe + "px";
+    rechtesPaddel.pos.y = rechtesPaddel.pos.y + paddelSpeed;
+    if (rechtesPaddel.pos.y + 128 + padding > window.innerHeight) {
+      rechtesPaddel.pos.y = window.innerHeight - 128 - padding;
     }
+    updatePaddelPosition(rechtesPaddel.pos.y);
   }
   if (pressedOben && !pressedUnten) {
-    const style = getComputedStyle(rechtesPaddel);
-    const alteHoehe = removePixel(style.top);
-    const neueHoehe = alteHoehe - paddelSpeed;
-    if (neueHoehe >= 0) {
-      const neueHoeheMitPixel = neueHoehe + "px";
-      rechtesPaddel.style.top = neueHoeheMitPixel;
-    } else {
-      rechtesPaddel.style.top = "0px";
+    rechtesPaddel.pos.y = rechtesPaddel.pos.y - paddelSpeed;
+    if (rechtesPaddel.pos.y < padding) {
+      rechtesPaddel.pos.y = padding;
     }
+    updatePaddelPosition(rechtesPaddel.pos.y, "rechts");
   }
 }
 
@@ -107,9 +106,51 @@ function init() {
 }
 
 function moveBall() {
-  console.log(ball.pos.x);
   ball.pos.x = ball.pos.x + ball.vec.x;
-  updateBallPosition(ball.pos.x + ball.vec.x);
+  ball.pos.y = ball.pos.y + ball.vec.y;
+  if (ball.pos.x > window.innerWidth) {
+    stop();
+    start();
+  }
+  if (ball.pos.x <= 0) {
+    ball.vec.x = -ball.vec.x;
+  }
+  if (ball.pos.y <= 0) {
+    ball.vec.y = -ball.vec.y;
+  }
+  if (ball.pos.y + ballSize >= window.innerHeight) {
+    ball.vec.y = -ball.vec.y;
+  }
+  if (ball.pos.x + 2 * ballSize >= window.innerWidth) {
+    if (
+      (ball.pos.y >= rechtesPaddel.pos.y &&
+        ball.pos.y <= rechtesPaddel.pos.y + paddelSize) ||
+      (ball.pos.y + ballSize >= rechtesPaddel.pos.y &&
+        ball.pos.y + ballSize <= rechtesPaddel.pos.y + paddelSize)
+    ) {
+      if (pressedUnten && !pressedOben && ball.vec.y >= 0) {
+        ball.vec.x = -ball.vec.x * xSpeedUp;
+        ball.vec.y = ball.vec.y * 1.3;
+        console.log("schneller", ball.vec.y, ball.vec.x);
+      } else if (pressedOben && !pressedUnten && ball.vec.y <= 0) {
+        ball.vec.x = -ball.vec.x * xSpeedUp;
+        ball.vec.y = ball.vec.y * 1.3;
+        console.log("schneller", ball.vec.y, ball.vec.x);
+      } else if (pressedOben && !pressedUnten && ball.vec.y >= 0) {
+        ball.vec.x = -ball.vec.x;
+        ball.vec.y = ball.vec.y * 0.8;
+        console.log("langsamer", ball.vec.y, ball.vec.x);
+      } else if (pressedUnten && !pressedOben && ball.vec.y <= 0) {
+        ball.vec.x = -ball.vec.x;
+        ball.vec.y = ball.vec.y * 0.8;
+        console.log("langsamer", ball.vec.y, ball.vec.x);
+      } else {
+        ball.vec.x = -ball.vec.x * xSpeedUp;
+        console.log("normal", ball.vec.x);
+      }
+    }
+  }
+  updateBallPosition(ball.pos.x, ball.pos.y);
 }
 
 function resetAll() {
@@ -119,8 +160,11 @@ function resetAll() {
 }
 
 function resetPaddel(seite) {
-  const halbeHoehe = window.innerHeight / 2;
-  updatePaddelPosition(halbeHoehe - 64, seite);
+  if (seite === "rechts") {
+    const halbeHoehe = window.innerHeight / 2;
+    rechtesPaddel.pos.y = halbeHoehe - 64;
+    updatePaddelPosition(halbeHoehe - 64, seite);
+  }
 }
 
 function resetBall() {
@@ -128,6 +172,8 @@ function resetBall() {
   const halbeWeite = window.innerWidth / 2;
   ball.pos.x = halbeWeite - 12;
   ball.pos.y = halbeHoehe - 12;
+  ball.vec.x = 0;
+  ball.vec.y = 0;
   updateBallPosition(ball.pos.x, ball.pos.y);
 }
 
@@ -145,6 +191,31 @@ function updatePaddelPosition(y, seite = "rechts") {
 }
 
 function start() {
-  resetBall();
-  ball.vec.x = paddelSpeed;
+  resetAll();
+  const yMin = -2;
+  const yMax = 2;
+  const xMax = 1.2;
+  const xMin = 0.8;
+  const xRandom = Math.random() * (xMax - xMin) + xMin;
+  const startVectorX = paddelSpeed * 0.8 * xRandom;
+  ball.vec.x = startVectorX;
+  const startVectorY = Math.random() * (yMax - yMin) + yMin;
+  ball.vec.y = startVectorY;
+  framesInterval = setInterval(update, fpsFaktor);
+  console.log("start", startVectorY, startVectorX);
+}
+
+function stop() {
+  clearInterval(framesInterval);
+  console.log("stop");
+}
+
+function pause() {
+  clearInterval(framesInterval);
+  console.log("pause");
+}
+
+function resume() {
+  framesInterval = setInterval(update, fpsFaktor);
+  console.log("weiter geht's");
 }
