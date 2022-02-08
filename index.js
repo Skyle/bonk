d8d0da833527f9fcbeb1e0772f6949454f3d8eac
Philipp Friedrich <freezer05@googlemail.com>
2022-02-08 22:47:30 +0100

    Konnentare und Feinheiten

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ resetButton.onclick = () => {
   start();
 };
 
+let counterElement = document.getElementById("counter");
+let speedElement = document.getElementById("speed");
+
 let framesInterval;
 runningCheckbox.onclick = (e) => {
   if (e.target.checked) {
@@ -40,11 +43,14 @@ const paddelSize = 128;
 const padding = 8;
 const ballSize = 24;
 const xSpeedUp = 1.05;
-const fpsFaktor = 16.666666666666668;
-const ySpeedUp = 1.36;
+const xSlow = 0.98;
+let fpsFaktor = 16.666666666666668;
+fpsFaktor = 15.4;
+const ySpeedUp = 1.24;
+let counter = 0;
+let ballSpeed = 0;
 
 window.onload = () => {
-  init();
   start();
 };
 
@@ -74,20 +80,31 @@ function obenlasser(e) {
     pressedUnten = false;
   }
 }
-
+// Die Hauptschleifenfunktion
+// Wird jede Aktualisierung aufgerufen und aktualisiert die Spieldaten
 function update() {
+  // Als erstes darf der Spieler sein Paddel bewergen
   moveRechtesPaddel();
   moveBall();
+  ballSpeed = calculateSpeed(ball.vec.x, ball.vec.y);
+  speedElement.innerHTML = ballSpeed;
 }
 
+// Benutzereingaben werden überprüft und dementsprechend das Passel bewegt
 function moveRechtesPaddel() {
+  // Es wird immer nur die Eingabe akzeptiert, bei der nur eine Taste gedrückt wird
+  // Falls die Pfeiltaste nach Unten gedrückt wird
   if (pressedUnten && !pressedOben) {
+    // Das Paddel wird um den paddelSpeed nach unten versetzt
     rechtesPaddel.pos.y = rechtesPaddel.pos.y + paddelSpeed;
+    // Falls das Paddel unten gegen den Fensterrand kommt, wird es auf den letzten möglichen Wert gesetzt
     if (rechtesPaddel.pos.y + 128 + padding > window.innerHeight) {
       rechtesPaddel.pos.y = window.innerHeight - 128 - padding;
     }
+    // Jetzt erfolgt die DOM-Manipulation
     updatePaddelPosition(rechtesPaddel.pos.y);
   }
+  // Und das gleiche nochmal um zu checken, ob das Paddel oben gegenkommt
   if (pressedOben && !pressedUnten) {
     rechtesPaddel.pos.y = rechtesPaddel.pos.y - paddelSpeed;
     if (rechtesPaddel.pos.y < padding) {
@@ -97,13 +114,10 @@ function moveRechtesPaddel() {
   }
 }
 
+// Entfernt das Pixel-Suffix aus einem String
+// vielleicht deprecated
 function removePixel(hoehe) {
   return parseFloat(hoehe.substring(0, hoehe.length - 2));
-}
-
-function init() {
-  console.log("Fertig geladen");
-  resetAll();
 }
 
 function moveBall() {
@@ -122,42 +136,46 @@ function moveBall() {
   if (ball.pos.y + ballSize >= window.innerHeight) {
     ball.vec.y = -ball.vec.y;
   }
+  // Ball und Paddel haben die gleiche x-Position
   if (ball.pos.x + 2 * ballSize >= window.innerWidth) {
+    // Ball trifft den Bereich der y-Achse auf der sich das Paddel befindet
+    // Damit treffen sich Ball und Paddel
+    // TODO Es können mehrere treffen direkt hintereinander erfolgen
     if (
       (ball.pos.y >= rechtesPaddel.pos.y &&
         ball.pos.y <= rechtesPaddel.pos.y + paddelSize) ||
       (ball.pos.y + ballSize >= rechtesPaddel.pos.y &&
         ball.pos.y + ballSize <= rechtesPaddel.pos.y + paddelSize)
     ) {
+      // Bei jedem Treffer wird er Counter um 1 erhöht
+      counter++;
+      counterElement.innerHTML = counter;
+      // Der Ball soll abhängig von der Bewergung des Paddels beschleunigt werden
+      // Es gibt 2 Checks, jeweils 1 für hoch und runter
+      // Ball und Paddel bewegen sich in die selbe Richtung
       if (pressedUnten && !pressedOben && ball.vec.y >= 0) {
         ball.vec.x = -ball.vec.x * xSpeedUp;
-        ball.vec.y = ball.vec.y * ySpeedUp + 2.5;
-        console.log("schneller", ball.vec.y, ball.vec.x);
+        ball.vec.y = ball.vec.y * ySpeedUp + 1.5;
       } else if (pressedOben && !pressedUnten && ball.vec.y <= 0) {
         ball.vec.x = -ball.vec.x * xSpeedUp;
-        ball.vec.y = ball.vec.y * ySpeedUp - 2.5;
-        console.log("schneller", ball.vec.y, ball.vec.x);
+        ball.vec.y = ball.vec.y * ySpeedUp - 1.5;
+        // Ball und Paddel bewegen sich in die entgegengesetzte Richtung
       } else if (pressedOben && !pressedUnten && ball.vec.y >= 0) {
-        ball.vec.x = -ball.vec.x * 0.95;
-        ball.vec.y = ball.vec.y * 0.8 - 1.6;
-        console.log("langsamer", ball.vec.y, ball.vec.x);
+        ball.vec.x = -ball.vec.x * xSlow;
+        ball.vec.y = ball.vec.y * 0.9 - 0.8;
       } else if (pressedUnten && !pressedOben && ball.vec.y <= 0) {
-        ball.vec.x = -ball.vec.x * 0.95;
-        ball.vec.y = ball.vec.y * 0.8 + 1.6;
-        console.log("langsamer", ball.vec.y, ball.vec.x);
+        ball.vec.x = -ball.vec.x * xSlow;
+        ball.vec.y = ball.vec.y * 0.9 + 0.8;
       } else {
         ball.vec.x = -ball.vec.x * xSpeedUp;
-        console.log("normal", ball.vec.x);
       }
     }
   }
   updateBallPosition(ball.pos.x, ball.pos.y);
 }
 
-function resetAll() {
-  resetBall();
-  resetPaddel("rechts");
-  resetPaddel("links");
+function calculateSpeed(x, y) {
+  return Math.sqrt(x ** 2 + y ** 2);
 }
 
 function resetPaddel(seite) {
@@ -190,11 +208,20 @@ function updatePaddelPosition(y, seite = "rechts") {
     linkesPaddel.element.style.top = y + "px";
   }
 }
+// Steuerfunktionen zur Animation
+
+function resetAll() {
+  resetBall();
+  resetPaddel("rechts");
+  resetPaddel("links");
+  counter = 0;
+  counterElement.innerHTML = 0;
+}
 
 function start() {
   resetAll();
-  const yMin = -2;
-  const yMax = 2;
+  const yMin = -3;
+  const yMax = 3;
   const xMax = 1.2;
   const xMin = 0.8;
   const xRandom = Math.random() * (xMax - xMin) + xMin;
@@ -203,20 +230,16 @@ function start() {
   const startVectorY = Math.random() * (yMax - yMin) + yMin;
   ball.vec.y = startVectorY;
   framesInterval = setInterval(update, fpsFaktor);
-  console.log("start", startVectorY, startVectorX);
 }
 
 function stop() {
   clearInterval(framesInterval);
-  console.log("stop");
 }
 
 function pause() {
   clearInterval(framesInterval);
-  console.log("pause");
 }
 
 function resume() {
   framesInterval = setInterval(update, fpsFaktor);
-  console.log("weiter geht's");
 }
